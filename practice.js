be423a0feffaed3f7481b0945a950d455157152e
Derek Scott <dered806@gmail.com>
2019-07-29 08:56:33 -0600

    partial finish, saving point

--- practice.js
+++ practice.js
@@ -28,6 +28,9 @@
 */
 
 // Code Here 
+function first(array, callback) {
+ callback(array[0])
+}
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -47,7 +50,10 @@ first(names, function(firstName){
   Then invoke the callback, passing in the last element in the array as the argument.
 */
 
-//Code Here
+function last(arr,cb) {
+  cb(arr[arr.length - 1])
+
+}
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -65,7 +71,9 @@ last(names, function(lastName){
   Invoke the callback, passing in the product of the two numbers multiplied as the argument. 
 */
 
-//Code Here
+function multiply(num1, num2, cb) {
+  cb (num1 * num2);
+}
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -85,6 +93,10 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
+function contains(arr,name,cb) {
+  cb (arr[arr.length], name)
+
+}
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -98,6 +110,8 @@ contains(names, 'Colt', function(result){
 
 
 
+
+
 ////////// PROBLEM 5 //////////
 
 /*
@@ -106,6 +120,11 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
+function uniq (array,cb) {
+  console.log(array) 
+  cb(array)
+}
+
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
@@ -122,7 +141,9 @@ uniq(names, function(uniqArr){
   For each name in the array, invoke the callback and pass in the name and the name's index as arguments.
 */
 
-//Code Here 
+function each (array, name, callback) {
+  callback(array, name)
+}
 
 // Do not edit the code below.
 each(names, function(item, indice){
@@ -140,6 +161,11 @@ each(names, function(item, indice){
 */
 
 // Code here
+function getUserById (users, id, callback) {
+callback()
+
+}
+
 
 // Do not edit the code below.
 var users = [
